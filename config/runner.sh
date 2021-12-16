c51a919b4c5ba1c5a04597d69ff0ae71f05860df
nicolasict <41732902+nicolasict@users.noreply.github.com>
2021-12-16 09:31:31 +0700

    Update runner.sh

--- config/runner.sh
+++ config/runner.sh
@@ -1,10 +1,11 @@
 # Tambah User
-useradd $SSH_USER --create-home --password "$(openssl passwd -1 "$SSH_PASSWORD")" && echo "$SSH_USER ALL=(ALL:ALL) ALL" >> /etc/sudoers
+#useradd $SSH_USER --create-home --password "$(openssl passwd -1 "$SSH_PASSWORD")" && echo "$SSH_USER ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # Timezone (Zona Waktu)
-rm -f /etc/localtime;  ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+#rm -f /etc/localtime;  ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 
 # Ownership
-chown -R $SSH_USER:$SSH_USER /var/www/html
+#chown -R $SSH_USER:$SSH_USER /var/www/html
 
-service nginx start && service php7.3-fpm start && service ssh $SSH_SERVICE
+#service nginx start && service php7.3-fpm start && service ssh $SSH_SERVICE
+service nginx start && service php7.3-fpm start
